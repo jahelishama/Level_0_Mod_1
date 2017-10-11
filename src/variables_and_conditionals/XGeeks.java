7a737adb1d6a2478d5e517b33869e69b1991491f
Syu125 <yusophia2011@gmail.com>
2017-10-10 19:16:49 -0700

    XGeeks Magic 8 ball

--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,10 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+		String superpower;
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+		superpower=JOptionPane.showInputDialog("What is your superpower?");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		JOptionPane.showMessageDialog(null,"My superpower is "+superpower+""  );
     }
 }
