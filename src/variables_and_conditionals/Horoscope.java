3d6d00af2b24fcbafc3c4987f0a120b798ac3831
Syu125 <yusophia2011@gmail.com>
2017-11-07 19:21:04 -0800

    Horoscope

---
+++ src/variables_and_conditionals/Horoscope.java
@@ -0,0 +1,16 @@
+package variables_and_conditionals;
+
+import javax.swing.JOptionPane;
+
+public class Horoscope{
+
+public static void main(String[] args) {
+String led=JOptionPane.showInputDialog("What month is your birthday");
+if(led.equals("October")){
+}
+if(led.equals("December")){
+if(led.equals("September")){
+if(led.equals("March")){
+if(led.equals("febuary)){
+}
+}
