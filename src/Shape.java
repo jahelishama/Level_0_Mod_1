63a16233de460a05ac10c1eb63599f7396e08428
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-17 19:15:34 -0700

    Shape

---
+++ src/Shape.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Shape {
+public static void main(String[] args) {
+	Robot randy= new Robot("mini");
+String Randy=JOptionPane.showInputDialog("How manny sides do you want");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
