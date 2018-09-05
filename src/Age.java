9a401d3159b7d792584e4e580849827bac697267
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-04 19:21:06 -0700

    ya

---
+++ src/Age.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Age {
+public static void main(String[] args) {
+	String age =JOptionPane.showInputDialog("How old are you");
+ int old2 =	Integer.parseInt(age);
+ if (old2>17) {
+ 
+	 String old3 =JOptionPane.showInputDialog("Who do you think the next president should be");
+	
+}else {
+
+	String old4 =JOptionPane.showInputDialog("nobody cares wahat you thinkn");
+
+}
+}
+}
+
