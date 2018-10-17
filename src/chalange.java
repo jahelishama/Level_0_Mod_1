9b92fd86b73d864cd8659766c86d0f9d829c02ad
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-16 19:25:45 -0700

    yah

---
+++ src/chalange.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class chalange {
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	String yeah =JOptionPane.showInputDialog("what number do  you want");
+	int you = Integer.parseInt(yeah);
+	
+	
+	for (int i = you; i >= 0; i--) {
+	
+		System.out.println(i);
+		
+		
+		
+	}
+}
+}
