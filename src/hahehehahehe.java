d4ad0ae7946579695b031d7236ea22ce109526de
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-30 19:10:58 -0700

    Needed help

---
+++ src/hahehehahehe.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class hahehehahehe {
+public static void main(String[] args) {
+	String yah =JOptionPane.showInputDialog("Typea number");
+	int number = Integer.parseInt(yah);
+	for (int i = 1; i <= number; i++) {
+	System.out.println(i);
+		}
+}
+}
